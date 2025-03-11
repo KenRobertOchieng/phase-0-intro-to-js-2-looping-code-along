8e07a7726617b83ab0d036e2414f0334f52f2651
Ken Robert Ochieng <kenrobertochi@gmail.com>
2025-03-11 09:00:03 +0300

    modified index.html

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []; // Step 1: Create an empty array
+  
+    for (let i = 0; i < names.length; i++) { // Step 2: Loop through the array
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+  
+    return messages; // Step 3: Return the new array
+  }
+  
+  // Example usage
+  console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--; // Decrease number in each iteration
+    }
+  }
+  
+  // Example usage
+  countDown(10);
+  
+  
+  
